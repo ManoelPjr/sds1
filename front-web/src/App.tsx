87262944df7a392617c5f3dd6196471b8587ac8a
ManoelPjr <napolesjr@gmail.com>
2020-09-24 23:46:12 -0300

    Add Records and Routes
    Added the necessary files to create the Records component and configure Routes

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import './App.css';
-import Header from './Components/Header/index';
-import Home from './pages/home';
+import Routes from './Routes';
+
 
 
 function App() {
   return (
     <div>
-      <Header />
-      <Home />
+      <Routes />
     </div>
 
   );
