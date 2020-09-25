192f5fbfea4a7808f829526429d78690aac69b91
ManoelPjr <napolesjr@gmail.com>
2020-09-24 22:34:38 -0300

    Add homepage
    Added the necessary files to create the home component

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
-import Header from './Components/Header/index.tsx';
+import Header from './Components/Header/index';
+import Home from './pages/home';
+
 
 function App() {
   return (
     <div>
       <Header />
+      <Home />
     </div>
 
   );
