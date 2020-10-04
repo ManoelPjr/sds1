780d785e2a3b462c2be793eafac5496f93e9f87a
ManoelPjr <napolesjr@gmail.com>
2020-10-03 23:15:27 -0300

    Preparing for Production
    Preparing the app to be put into production in heroku.

--- front-web/src/pages/records/index.tsx
+++ front-web/src/pages/records/index.tsx
@@ -6,7 +6,7 @@ import { formatDate } from './helpers';
 import Pagination from './pagination';
 import  Filters  from '../../Components/Filters';
 
-const BASE_URL = 'http://localhost:8080'
+const BASE_URL = 'https://sds1-manoel.herokuapp.com'
 
 const Records = () => {
     const [recordsResponse, setRecordsResponse] = useState<RecordsResponse>();
